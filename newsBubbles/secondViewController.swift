2cc556bc180307c4a0b0f62561c41c3425408fe2
kevin <k87158@gmail.com>
2016-03-14 22:51:15 +0800

    new secondViewController cocoa class

---
+++ newsBubbles/secondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  secondViewController.swift
+//  newsBubbles
+//
+//  Created by kevin on 2016/3/14.
+//  Copyright © 2016年 kevin. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+   
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
